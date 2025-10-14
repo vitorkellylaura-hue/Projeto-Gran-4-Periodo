0cc4f56d5281802b2fe138e16d87675d14d0a4cc
Paulo Vitor <vitorkellylaura@gmail.com>
2025-10-14 14:50:56 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    res.end('Olá, Mundo!');
+
+});
+
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+
+    console.log(`Servidor rodando em http://localhost:${PORT}/`);
+
+});
